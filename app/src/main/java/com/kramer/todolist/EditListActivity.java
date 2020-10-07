ac7e72f8d54fc9899e05b4dc72cd6d07a7ee94f7
piotrek2302 <49973318+piotrek2302@users.noreply.github.com>
2020-10-07 17:24:55 +0200

    Første opplastning

---
+++ app/src/main/java/com/kramer/todolist/EditListActivity.java
@@ -0,0 +1,14 @@
+package com.kramer.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_list);
+    }
+}
---
+++ app/src/main/java/com/kramer/todolist/ViewListActivity.java
@@ -0,0 +1,14 @@
+package com.kramer.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_list);
+    }
+}
